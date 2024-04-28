ffa93d04445e3726e839e21dc079f559d223006e
franklinAnozie <cfanozie@gmail.com>
2024-04-28 17:49:14 +0100

    testing actions

--- db/storage.py
+++ db/storage.py
@@ -8,6 +8,7 @@ from os import getenv
 
 load_dotenv()
 
+
 class Storage(object):
     __engine = None
     __session = None
@@ -15,13 +16,13 @@ class Storage(object):
     def __init__(self):
         url_object = URL.create(
             "mysql+mysqlconnector",
-            username = getenv("DB_USERNAME"),
-            password = getenv("DB_PASSWORD"),
-            host = getenv("DB_HOST"),
-            database = getenv("DB_DATABASE"),
+            username=getenv("DB_USERNAME"),
+            password=getenv("DB_PASSWORD"),
+            host=getenv("DB_HOST"),
+            database=getenv("DB_DATABASE"),
         )
         self.__engine = create_engine(url_object)
-    
+
     def all(self, cls=None):
         objs = {}
         if cls is not None:
@@ -30,20 +31,20 @@ class Storage(object):
         else:
             pass
         return objs
-    
+
     def new(self, obj):
         self.__session.add(obj)
-    
+
     def save(self):
         self.__session.commit()
-    
+
     def delete(self, obj):
         self.__session.delete(obj)
         self.__session.commit()
-    
+
     def close(self):
         self.__session.close()
-    
+
     def reload(self):
         Base.metadata.create_all(self.__engine)
         session = sessionmaker(bind=self.__engine, expire_on_commit=False)
--- models/Model.py
+++ models/Model.py
@@ -1,12 +1,14 @@
 #!./new_env/bin/python3
 
 from datetime import datetime
-from sqlalchemy import Column, DATETIME, String, Integer
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, DATETIME, String, Integer, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 from uuid import uuid4
-import  models
+import models
+
 
 Base = declarative_base()
+
 
 class Model(object):
     def __init__(self, **kwargs):
@@ -18,27 +20,29 @@ class Model(object):
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-    
+
     def save(self):
         models.storage.new(self)
         models.storage.save()
-    
+
     def delete(self):
         models.storage.delete(self)
+
 
 class Users(Model):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-    
+
     def get_full_name(self):
-       return f"{self.first_name} {self.last_name}"
-    
+        return f"{self.first_name} {self.last_name}"
+
     def get_user_name(self):
-       return f"{self.user_name}"
-    
+        return f"{self.user_name}"
+
     def change_password(self, new_password):
         self.password = new_password
         return f"{self.password}"
+
 
 class Customer(Users, Base):
 
@@ -54,43 +58,56 @@ class Customer(Users, Base):
     user_name = Column(String(256))
     password = Column(String(256))
     fine = Column(Integer)
-    borrowed_books = Column(String(256))
+    borrowed_books = relationship("Book", secondary='customer_books')
     role = Column(String(60))
 
     __limit = 3
-    __borrowed_book = {}
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-    
-    def get_num_of_borrowed(self):
-        return len(self.__borrowed_book)
-    
-    ##def get_borrowed(self):
-        ##user_info = 
 
-    
-    def borrow_book(self, new_book):
-        if len(self.__borrowed_book) <= self.__limit:
-            self.__borrowed_book[new_book["id"]] = new_book
+    def get_num_of_borrowed(self):
+        return len(self.borrowed_books)
+
+    def get_borrowed(self):
+        return self.borrowed_books
+
+    def borrow_book(self, book_id):
+        lst = self.borrowed_books
+        if len(lst) < self.__limit or lst is None:
+            self.borrowed_books.append(book_id)
+            models.storage.save()
             return True
         else:
             return False
-    
-    def return_book(self, ret_book):
-        for key, value in self.__borrowed_book.items():
-            if key == ret_book["id"]:
-                del(self.__borrowed_book[ret_book["id"]])
-                return True
-    #def pay_fine()
-    #def create_account()
-    #def delete_account()
-    #def update_account()
+
+    def return_book(self, book_id):
+        self.borrowed_books.delete(book_id)
+        models.storage.save()
+        return True
+
+    def pay_fine(self, amount):
+        all_customers = models.storage.all(Customer)
+        for key, value in all_customers.items():
+            if key == self.id:
+                paid_fine = value["fine"] - amount
+                self.fine = paid_fine
+                models.storage.save()
+
+    def delete_account(self):
+        models.storage.delete(self)
+
+    def update_account(self, update_details):
+        for key, value in update_details:
+            self[key] = value
+        models.storage.save()
+
     def fine_customer(self, fine):
         self.fine = fine
 
+
 class Staff(Users, Base):
-    
+
     __tablename__ = "staff"
     __table_args__ = {'mysql_engine': 'InnoDB', 'mysql_charset': 'latin1'}
 
@@ -106,14 +123,13 @@ class Staff(Users, Base):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-    
+
     def add_user(self, **user_data):
+        """ method to create a new user """
         if user_data["role"] == "Customer":
             new_customer = Customer(**user_data)
             new_customer.save()
-        else:
-            raise Exception("Can't create users")
-    
+
     def delete_user(self, user_id):
         all_users = models.storage.all(Customer)
         for key, value in all_users.items():
@@ -129,18 +145,18 @@ class Staff(Users, Base):
                 value.fine_customer(fine)
                 models.storage.save()
                 break
-    
+
     def add_book(self, **book_details):
         new_book = Books(**book_details)
         new_book.save()
-    
+
     def remove_book(self, book_id):
         all_books = models.storage.all(Books)
         for key, value in all_books.items():
             if key == book_id:
                 value.delete()
                 break
-    
+
     def update_book(self, **book_details):
         all_books = models.storage.all(Books)
         for key, value in all_books.items():
@@ -150,10 +166,11 @@ class Staff(Users, Base):
                 value.update_book()
                 break
 
+
 class Admin(Staff):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-    
+
     def add_user(self, **user_data):
         if user_data["role"] == "Admin":
             new_admin = Admin(**user_data)
@@ -164,7 +181,7 @@ class Admin(Staff):
         else:
             new_customer = Customer(**user_data)
             new_customer.save()
-    
+
     def delete_user(self, **kwargs):
         if kwargs["role"] == "Admin":
             all_users = models.storage.all(Admin)
@@ -183,7 +200,8 @@ class Admin(Staff):
             for key, value in all_users.items():
                 if key == kwargs["id"]:
                     value.delete()
-                    break 
+                    break
+
 
 class Books(Model, Base):
 
@@ -200,6 +218,16 @@ class Books(Model, Base):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-    
+
     def update_book(self, ):
-       models.storage.save()
+        models.storage.save()
+
+
+class CustomerBook(Base):
+
+    __tablename__ = 'customer_books'
+    __table_args__ = {'mysql_engine': 'InnoDB', 'mysql_charset': 'latin1'}
+
+    id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    book_id = Column(Integer, ForeignKey('books.id'))
