827965dc48e34cfaea5e28d1670043c1fc235840
franklinAnozie <cfanozie@gmail.com>
2024-04-28 17:49:14 +0100

    testing actions

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+#!./new_env/bin/python3
+
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
