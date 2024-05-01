fba8d554c460338982975135dfde8eb98d4eeed2
Sphax <itssphax123@gmail.com>
2024-05-01 16:22:43 +0100

    fixed pycodestyle

--- app.py
+++ app.py
@@ -10,13 +10,16 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
