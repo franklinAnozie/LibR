78749aa27aa3693c70d2a5dd7d56439f54daa0ea
Sphax <itssphax123@gmail.com>
2024-05-06 19:01:49 +0100

    landing page 1.0

--- app.py
+++ app.py
@@ -21,5 +21,10 @@ def signup():
     return render_template('signup.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
