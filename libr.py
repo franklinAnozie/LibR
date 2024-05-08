c6a3c6430d2ed716d16b06c3df23dcf4295080cc
Sphax <itssphax123@gmail.com>
2024-05-08 10:13:35 +0100

    landing page 2.0

---
+++ libr.py
@@ -0,0 +1,30 @@
+#!./new_env/bin/python3
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
